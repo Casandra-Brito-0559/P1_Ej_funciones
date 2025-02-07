abaec1bfa2bede0254ac561c648c0a2deef8cceb
Casandra Brito Gomez <164533984+Casandra-Brito-0559@users.noreply.github.com>
2025-02-07 16:50:01 +0000

    mi primera vez

--- funcion1.dart
+++ funcion1.dart
@@ -3,13 +3,15 @@
 void saludar(){
   print('hola mundo');
 }
+
 void escribeNombre(){
   print('Casandra Brito');
 }
+
 void main(){
   //Casandra Brito 22308051280559
   // llamar a la funcion saludar
-  
+
   print('Casandra Brito 22308051280559');
   print('Llamando a la funcion saludar');
   saludar();
