69cdfecb5c28873212efe615a12d5b8032a4dd97
Casandra Brito Gomez <164533984+Casandra-Brito-0559@users.noreply.github.com>
2025-02-07 16:47:42 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,17 @@
+// funcio que no devuelve valor
+// funcion saludar
+void saludar(){
+  print('hola mundo');
+}
+void escribeNombre(){
+  print('Casandra Brito');
+}
+void main(){
+  //Casandra Brito 22308051280559
+  // llamar a la funcion saludar
+  print('Casandra Brito 22308051280559');
+  print('Llamando a la funcion saludar');
+  saludar();
+  print('Llamando a la funcion escribeNombre');
+  escribeNombre();
+}
