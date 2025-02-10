696377c9e122a1f74b9f29982dbb3eb88e0d5628
Casandra Brito Gomez <164533984+Casandra-Brito-0559@users.noreply.github.com>
2025-02-10 18:50:02 +0000

    Funcion parametro

---
+++ funcionparam.dart
@@ -0,0 +1,20 @@
+
+  int sumar(int a, int b) {
+    return a + b;
+  }
+
+  // Definición de la función saludar dentro de main
+  saludar(String nombre, int edad) {
+    print('Hola, $nombre. Tienes $edad años.');
+  }
+
+  // Llamada a la función sumar
+  void main(){ 
+    print('Casandra Brito Gomez Mat: 22308051280559');
+    int resultadoSuma = sumar(5, 3);
+  print('La suma es: $resultadoSuma');
+
+  // Llamada a la función saludar
+  saludar('Casandra', 17);
+  }
+ 
--- recursos/funcionsum.md
+++ recursos/funcionsum.md
@@ -3,3 +3,6 @@ Ejemplo de salida
 
 Ejemplo de salida funcion normal + flecha
 ![alt text](image-1.png)
+
+Ejemplo de salida funcion con parametro
+![alt text](image-2.png)
