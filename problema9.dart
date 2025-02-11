09e88484c0d145bf967750b8532bbad59a9177c8
Casandra Brito Gomez <164533984+Casandra-Brito-0559@users.noreply.github.com>
2025-02-11 20:24:38 +0000

    problema9

---
+++ problema9.dart
@@ -0,0 +1,24 @@
+void main() {
+  // Capturar la lista de números
+  List<int> lista = capturarL();
+
+  // Ordenar la lista de menor a mayor
+  lista.sort();
+
+  // Mostrar la lista ordenada
+  mostrarL(lista);
+}
+
+// Función para capturar la lista de números
+List<int> capturarL() {
+  // Lista predefinida de números enteros
+  return [34, 7, 23, 32, 5, 62];
+}
+
+// Función para mostrar la lista
+void mostrarL(List<int> lista) {
+  print("Lista ordenada de menor a mayor:");
+  for (int numero in lista) {
+    print(numero);
+  }
+}
---
+++ recursos/problema9.md
@@ -0,0 +1,7 @@
+Redaccion:
+
+9 Escribe un programa que tome una lista de números enteros (int) y la ordene de menor a mayor.
+
+Salida de la lista en orden:
+
+![alt text](image-4.png)
